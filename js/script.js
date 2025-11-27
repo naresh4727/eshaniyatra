a9099046eafbdef32d5f9ada51493a9de2fce5bd
naresh4727 <109799023+naresh4727@users.noreply.github.com>
2025-11-28 00:12:32 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -78,7 +78,32 @@ $(document).ready(function () {
     ]
 });
 
-
+ $('.testimonial-slider').slick({
+            dots: true,
+            arrows: false,
+            infinite: true,
+            speed: 500,
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            autoplay: true,
+            autoplaySpeed: 3000,
+            responsive: [
+                {
+                    breakpoint: 1024,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 1
+                    }
+                },
+                {
+                    breakpoint: 768,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1
+                    }
+                }
+            ]
+        });
 
 });
 
